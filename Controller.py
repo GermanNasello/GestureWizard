8c6ca4a6c6664ef0a19e1789498b67dfb2e9b667
GermanNasello <150060650+GermanNasello@users.noreply.github.com>
2023-11-07 16:43:19 +0100

    Corregido fallos en Controller y añadida funcion set

--- Controller.py
+++ Controller.py
@@ -1,15 +1,28 @@
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
+
 Class Controller():
+  def __init__():
+    self.devices = AudioUtilities.GetSpeakers()
+    self.interface = self.devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+    self.volume = cast(self.interface, POINTER(IAudioEndpointVolume))
+
+  def set_volumen(v):
+    self.volume.SetMasterVolumeLevelScalar(v,None)
+
   def mute():
-      current_volume = volume.GetMasterVolumeLevelScalar()
+      current_volume = self.volume.GetMasterVolumeLevelScalar()
       new_volume = 0
-      volume.SetMasterVolumeLevelScalar(new_volume, None)
+      self.volume.SetMasterVolumeLevelScalar(new_volume, None)
   
   def subir_volumen():
-      current_volume = volume.GetMasterVolumeLevelScalar()
+      current_volume = self.volume.GetMasterVolumeLevelScalar()
       new_volume = current_volume+0.35  # 70% del volumen máximo
-      volume.SetMasterVolumeLevelScalar(new_volume, None)
+      self.volume.SetMasterVolumeLevelScalar(new_volume, None)
   
   def bajar_volumen():
-      current_volume = volume.GetMasterVolumeLevelScalar()
+      current_volume = self.volume.GetMasterVolumeLevelScalar()
       new_volume = current_volume-0.35# 70% del volumen máximo
-      volume.SetMasterVolumeLevelScalar(new_volume, None)
+      self.volume.SetMasterVolumeLevelScalar(new_volume, None)
