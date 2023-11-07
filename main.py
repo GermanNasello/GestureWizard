681e5b9004b3bb345eb4a854a497e85bbebddbfe
GermanNasello <150060650+GermanNasello@users.noreply.github.com>
2023-11-07 16:43:38 +0100

    Main propuesto para funcionar con clases

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import cv2
 import mediapipe as mp
-import math
-from ctypes import cast, POINTER
-from comtypes import CLSCTX_ALL
-from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+import Controller
+import HandProcesor
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
@@ -17,53 +15,8 @@ devices = AudioUtilities.GetSpeakers()
 interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
 volume = cast(interface, POINTER(IAudioEndpointVolume))
 
-def distance(v1,v2,p1,p2):
-    return math.sqrt(math.pow(v1.landmark[p1].x - v2.landmark[p2].x, 2) + math.pow(v1.landmark[p1].y - v2.landmark[p2].y, 2))
-def mute():
-    current_volume = volume.GetMasterVolumeLevelScalar()
-    new_volume = 0
-    volume.SetMasterVolumeLevelScalar(new_volume, None)
-
-def subir_volumen():
-    current_volume = volume.GetMasterVolumeLevelScalar()
-    new_volume = current_volume+0.35  # 70% del volumen máximo
-    volume.SetMasterVolumeLevelScalar(new_volume, None)
-
-def bajar_volumen():
-    current_volume = volume.GetMasterVolumeLevelScalar()
-    new_volume = current_volume-0.35# 70% del volumen máximo
-    volume.SetMasterVolumeLevelScalar(new_volume, None)
-
-def comprobar_mano(m):
-    res=""
-
-    if distance(m, m, 4, 17) < distance(m, m, 2, 4):
-        res+='1'
-    else:
-        res+='0'
-
-    if distance(m, m, 0, 8) < distance(m, m, 0, 6):
-        res+= '1'
-    else:
-        res+= '0'
-
-    if distance(m, m, 0, 12) < distance(m, m, 0, 9):
-        res+= '1'
-    else:
-        res+= '0'
-
-    if distance(m, m, 0, 16) < distance(m, m, 0, 13):
-        res+= '1'
-    else:
-        res+= '0'
-
-    if distance(m, m, 0, 20) < distance(m, m, 0, 17):
-        res+= '1'
-    else:
-        res+= '0'
-
-    return res
-
+controller = Controller()
+hp = HandProcesor()
 
 contador=dict()
 funcionando=None
@@ -71,7 +24,7 @@ humbral_inicio=2
 humbral_fin=2
 
 return_counter=humbral_fin
-posiciones=[]
+
 while True:
     data, image = cap.read()
     image = cv2.cvtColor(cv2.flip(image,1),cv2.COLOR_BGR2RGB)
@@ -79,26 +32,9 @@ while True:
     resultspose = pose.process(image)
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     if resultsmanos.multi_hand_landmarks:
-        for hand_landmarks in resultsmanos.multi_hand_landmarks:
-            mp_drawing.draw_landmarks(
-                image,
-                hand_landmarks,
-                mphands.HAND_CONNECTIONS
-            )
-        alto, ancho , c = image.shape
-        #print(image.shape)
-        i=0
-
-        for hand_landmarks in resultsmanos.multi_hand_landmarks:
-            for j in range(21):
-                px = hand_landmarks.landmark[j].x * ancho
-                py = hand_landmarks.landmark[j].y * alto
-                #print(int(hand_landmarks.landmark[0].x*100))
-                #image = cv2.putText(image,str(j),(int(px),int(py)), cv2.FONT_HERSHEY_SIMPLEX, 0.2,
-                      #(0, 0, 255), 1, cv2.LINE_AA)
-            i+=1
-
-
+        
+        image = hp.mostrarPuntos()
+        
         if funcionando!=None :
             master=None
             for mano in resultsmanos.multi_hand_landmarks:
@@ -106,39 +42,39 @@ while True:
                     master = mano
                     break
             if(master==None):
-                return_counter-=1
-                if return_counter<=0:
+                
+                if ((return_counter:=return_counter-1)<=0):
                     if funcionando == "00110":
-                        mute()
+                        controller.mute()
                         funcionando = None
-                        posiciones=[]
+                        hp.posiciones=[]
                     else:
                         funcionando = None
                         print("Fin de trackeo, procesar movimiento")
-                        movimiento=[0,0]
-                        for i in range(1,len(posiciones)):
-                            movimiento[0] += posiciones[i][0]-posiciones[i-1][0]
-                            movimiento[1] += posiciones[i][1]-posiciones[i-1][1]
+                        movimiento = hp.procesarMovimiento()
                         print(movimiento)
-                        posiciones=[]
-                        if movimiento[0]<0:
-                            print("bajar el volumen")
-                            try:
-                                bajar_volumen()
-                            except:
-                                print("Esta al minimo")
-                        else:
-                            print("subir el volumen")
-                            try:
-                                subir_volumen()
-                            except:
-                                print("Esta al maximo")
 
+                        if(movimiento=="derecha"):
+                            try:
+                                controller.subir_volumen()
+                            else:
+                                controller.set_volumen(1)
+                                print("Está al maximo")
+
+                        elif(movimiento=="izquierda"):
+                            try:
+                                controller.bajar_volumen()
+                            else:
+                                controller.set_volumen(0)
+                                print("Está al minimo")
+                        else:
+                            print(movimiento)
                     
             else:
                 print("Guardando posicion")
-                posiciones.append([master.landmark[0].x,master.landmark[0].y])
+                hp.guardarPosicion(master, 0)
                 return_counter=humbral_fin
+                
         if funcionando==None:
             for mano in resultsmanos.multi_hand_landmarks:
                 pos = comprobar_mano(mano)
